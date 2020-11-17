c7ecb72864e784ddb7c18547c27b0fb12b6af99d
DENNIS MWANGI <dennistrevor06@gmail.com>
2020-11-17 21:52:36 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let container = document.querySelector('.conatiner');
+let container = document.querySelector('.container');
 let div;
 let maxSize = 550;
 let nCells = 16;
@@ -7,33 +7,32 @@ let penColor = '#000';
 let counter;
 
 const redpen = document.querySelector('#red-pen');
-const black = document.querySelector('#black-pen');
+const blackpen = document.querySelector('#black-pen');
 const rainbowPen = document.querySelector('#rainbow-pen');
 const pencil = document.querySelector('#pencil')
 
 // Get the size of the grid Cells
 function getSizeOfCells(maxSize, nCells) {
   //set up local version of variable
-  let sizeOfCells; 
-
-    sizeOfCells = maxSize / nCells;
-    sizeOfCells = sizeOfCells + 'px';
-    return sizeOfCells;
+  let sizeOfCells;
+  sizeOfCells = maxSize / nCells;
+  sizeOfCells = sizeOfCells + 'px';
+  return sizeOfCells;
 }
 
-//Generating the grid drawing 
+// Generating the drawing grid
 function makeGrid(nCells) {
-  sizeOfCells = getSizeOfcells(maxSize, nCells);
+  sizeOfCells = getSizeOfCells(maxSize, nCells);
 
   container.style['grid-template'] =
-  `repeat(${nCells}, ${sizeOfCells}) / repeat(${nCells}, ${sizeOfCells})`;
+    `repeat(${nCells}, ${sizeOfCells}) / repeat(${nCells}, ${sizeOfCells})`;
 
   for (let i = 0; i < nCells; i++) {
-    for (let j = 0; j < ncells; j++) {
+    for (let j = 0; j < nCells; j++) {
       div = document.createElement('div');
       div.classList.add('cell');
       div.setAttribute('data-counter', 10);
-      container.appendChild(div); 
+      container.appendChild(div);
     }
   }
 }
@@ -43,78 +42,99 @@ function draw() {
   grid = document.querySelectorAll('.container > .cell');
 
   for (let i = 0; i < grid.length; i++) {
-    grid[i].addEventListener('mouseover', function() {
-      grid[i].style.backgroundColor = pencolor;
+    grid[i].addEventListener('mouseover', function () {
+      grid[i].style.backgroundColor = penColor;
     });
   }
 }
 
 // Reset the grid color to white
 function clearGrid() {
-  for(let i = 0; i < grid.length; i++) {
-    for(let i = 0; i < grid.length; i++) {
-      grid[i].style.backgroundColor = '#FFF'
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].style.backgroundColor = '#FFF';
+  }
+}
+
+// creates a new grid with diffrent  sized Cells
+function replaceGrid() {
+  ncells = prompt('Enter a number between 1 and 50');
+  if (nCells > 50 || nCells < 1) {
+    if (typeof (nCells) != 'object') {
+      alert("that's not Within range");
     }
+  } else {
+    //Remove old grid
+    while (container.hasChildNodes()) {
+      container.removeChild(container.firstChild);
+    }
+    // Establish new grid
+    makeGrid(nCells);
+    draw();
+  }
+}
+
+//Randomize colors
+function getRandomColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  penColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+
+}
+
+// Adds a layer of shade
+function shadeColor() {
+  counter = this.getAttribute('data-counter');
+  counter -= 1;
+  this.style.filter = `brightness(${counter * 10}%)`;
+  this.setAttribute('data-counter', counter);
+}
+
+// switches to black pen when button is clicked
+redpen.addEventListener('click', function () {
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].removeEventListener('mouseover', shadeColor);
+    grid[i].removeEventListener('mouseover', getRandomColor);
   }
 
-  // creates a new grid with diffrent  sized Cells
-  function replaceGrid() {
-    ncells = prompt('Enter a number between 1 and 50');
-    if (nCells > 50 || nCells <1) {
-      if (typeof(nCells) != 'object') {
-        alert("that's not Within range");
-      }
-    } else {
-      //Remove old grid
-      while (container.hasChildNodes()) {
-        container.removeChild(container.firstChild);
-      } 
-// Establish new grid
+  pencolor = '#FF0000';
+  draw();
+});
+
+// Switches to black pen when button is clicked
+
+blackpen.addEventListener('click', function () {
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].removeEventListener('mouseover', shadeColor);
+    grid[i].removeEventListener('mouseover', getRandomColor);
+  }
+
+  penColor = '#000';
+  draw();
+});
+
+
+//switches to rainbow pen when button is clicked
+rainbowPen.addEventListener('click', function () {
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].removeEventListener('mouseover', shadeColor);
+    grid[i].addEventListener('mouseover', getRandomColor);
+  }
+  draw();
+});
+
+// Switches to shade pen when button is clicked
+pencil.addEventListener('click', function () {
+  counter = 0;
+  penColor = 'rgba(250, 250, 250, 1)';
+
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].removeEventListener('mouseover', getRandomColor);
+    grid[i].addEventListener('mouseover', shadeColor);
+  }
+  draw();
+});
+
 makeGrid(nCells);
 draw();
-    }
-  }
-
-  //Randomize colors
-  function getRandomColor() {
-    let r = Math.floor(Math.random()*256);
-    let g = Math.floor(Math.random()*256);
-    let b = Math.floor(Math.random()*256);
-
-    penColor = 'rgb('+r+','+g+','+b+')';
-
-  }
-
-  //Adds a layer of shade
-
-  function shadeColor() {
-    counter = this.getAttributes('data-counter');
-    counter -= 1;
-    this.style.filter= `brightness(${counter*10}%)`;
-    this.setAttribute('data-counter', counter);
-  }
-
-  // switches to black pen when button is clicked
-
-  redPen.addEventListener('click', function() {
-    for (let i = 0; i < grid.length; i++) {
-      grid[i].removeEventListener('mouseover', shadeColor);
-      grid[i].removeEventListener('mouseover', getRandomColor);
-      }
-
-      pencolor = '#FF0000';
-      draw();
-  });
-
-  //switches to rainbow pen when button is clicked
-  rainbowPen.addEventListener('click', function() {
-    for (let i = 0; i < grid.length; i++) {
-      grid[i].removeEventListener('mouseover', shadeColor);
-      grid[i].addEventListener('mouseover', getRandomColor);
-    }
-    draw();
-  });
-  makeGrid(nCells);
-  draw();
-}
-  
